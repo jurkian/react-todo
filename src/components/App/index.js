34f9abfd6b8bc76f513f5dc2b7c84cbdb3839a3a
Jakub Jurkian <jakub@jurkian.com>
2017-07-26 16:59:29 +0200

    Prepare a project structure

---
+++ src/components/App/index.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import './app.css';
+
+class App extends Component {
+   constructor() {
+      super();
+   }
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import 'normalize.css';
+import './index.css';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App/index';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
