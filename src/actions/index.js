5aefa07fb0d28d15045900c1d220bd417297ca22
Jakub Jurkian <jakub.jurkian@gmail.com>
2017-11-26 14:53:55 +0100

    Prepare actions and action creators

---
+++ src/actions/index.js
@@ -0,0 +1,34 @@
+// Action types
+export const ADD_TODO = 'ADD_TODO';
+export const TOGGLE_TODO = 'TOGGLE_TODO';
+export const REMOVE_TODO = 'REMOVE_TODO';
+export const CHANGE_VISIBILITY_FILTER = 'CHANGE_VISIBILITY_FILTER';
+
+// Action creators
+export const addTodo = (text) => {
+   return {
+      type: ADD_TODO,
+      text
+   }
+}
+
+export const toggleTodo = (id) => {
+   return {
+      type: TOGGLE_TODO,
+      id
+   }
+}
+
+export const removeTodo = (id) => {
+   return {
+      type: REMOVE_TODO,
+      id
+   }
+}
+
+export const setVisibilityFilter = (filter) => {
+   return {
+      type: CHANGE_VISIBILITY_FILTER,
+      filter
+   }
+}
