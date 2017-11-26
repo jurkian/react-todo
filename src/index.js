d062006d125bc779d683cd66cb38b2d183bd40a7
Jakub Jurkian <jakub.jurkian@gmail.com>
2017-11-26 15:28:56 +0100

    Connect Redux store, allow debugging

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
-import './polyfills.js';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import reducers from './reducers';
 
+import './polyfills.js';
 import 'normalize.css';
 import './index.css';
 
-import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './components/App/index';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(
+   reducers,
+   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+ReactDOM.render(
+   <Provider store={store}>
+      <App />
+   </Provider>,
+   document.getElementById('root')
+);
+
 registerServiceWorker();
