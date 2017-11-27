bab05876cf822bf7b3b72e28cd4da9f3454c0eca
Jakub Jurkian <jakub.jurkian@gmail.com>
2017-11-28 00:46:49 +0100

    Redux: clean the App component

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './app.css';
 
 import AddTodo from '../AddTodo';
@@ -6,87 +6,14 @@ import TodosList from '../../containers/TodosList';
 import FilterLinks from '../../containers/FilterLinks';
 import SearchBox from '../SearchBox';
 
-class App extends Component {
-   constructor() {
-      super();
+const App = () => (
+   <div className="App">
+      <AddTodo />
+      <TodosList />
+      <FilterLinks />
+      <SearchBox />
+   </div>
 
-      this.state = { tasks: [], search: '' };
-   }
-
-   componentWillMount() {
-      fetch('http://localhost:3001/tasks').then(res => {
-         return res.json();
-      }).then(tasks => {
-         this.setState({ tasks });
-      });
-   }
-
-   render() {
-      return (
-         <div className="App">
-            <AddTodo />
-            <TodosList />
-            <FilterLinks />
-            <SearchBox />
-         </div>
-      )
-   }
-
-   toggleTaskComplete(task) {
-      fetch(`http://localhost:3001/tasks/${task.id}`, {
-         method: 'PUT',
-         headers: { 'Content-Type': 'application/json' },
-         body: JSON.stringify({
-            'title': task.title,
-            'complete': !task.complete
-         })
-      }).then(() => {
-
-         const currentTask = this.state.tasks.find(el => el.title === task.title);
-         currentTask.complete = !currentTask.complete;
-
-         // Refresh state
-         this.setState({ tasks: this.state.tasks });
-      });
-   }
-
-   removeTask(id) {
-      fetch(`http://localhost:3001/tasks/${id}`, {
-         method: 'DELETE'
-      }).then(() => {
-
-         // Refresh state
-         this.setState(prevState => ({ tasks: prevState.tasks.filter(task => task.id !== id) }));
-      });
-   }
-
-   addNewTask(title) {
-      if (title.length >= 2) {
-         fetch('http://localhost:3001/tasks', {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({
-               'title': title,
-               'complete': false
-            })
-         }).then(() => {
-
-            // Add item to state
-            this.state.tasks.push({
-               title,
-               complete: false
-            });
-
-            // Refresh state
-            this.setState({ tasks: this.state.tasks });
-         });
-      }
-   }
-
-   findTask(title) {
-      // Always update state, even if it goes back to empty (default state)
-      this.setState({ search: title })
-   }
-}
+);
 
 export default App;
